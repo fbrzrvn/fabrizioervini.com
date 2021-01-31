d250915ebed4f6debb21a6af646b1d4f47e7b825
fabrizio <faber.rvn@gmail.com>
2021-01-31 21:18:34 +0100

    Add About and Blog pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} exact />
+        <Route path="/blog" component={Blog} exact />
         <Route path="/contact" component={Contact} exact />
       </Switch>
     </Router>
