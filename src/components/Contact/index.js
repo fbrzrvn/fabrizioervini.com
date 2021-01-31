5d16ea5bcbe97be52f84f01133ec0d08cbfd9b65
fabrizio <faber.rvn@gmail.com>
2021-01-31 21:16:30 +0100

    Create Contact page # Add form validation # Add emailjs api # Add success page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Form from './Form';
+import Success from './Success';
+import {
+  Container,
+  FormWrapper,
+  Icon } from './ContactElements';
+
 
 const Contact = () => {
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <>
+      <Container>
+        <FormWrapper>
+          <Icon to="/">faber</Icon>
+          {!isSubmitted ? <Form submitForm={submitForm} /> : <Success />}
+        </FormWrapper>
+      </Container>
+    </>
   )
 }
 
