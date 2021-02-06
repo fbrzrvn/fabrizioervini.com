806955f94b3a8068fc79afade19593ac70da6d4b
fabrizio <faber.rvn@gmail.com>
2021-02-06 13:41:19 +0100

    Add About Text and Image

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -13,6 +13,8 @@ export const Container = styled.div`
 
 export const AboutWrapper = styled.div`
   height: 100%;
+  width: 90vw;
+  margin: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -26,9 +28,9 @@ export const Icon = styled(LinkRouter)`
   font-weight: 700;
   text-decoration: none;
   color: #61dafb;
-  padding: 32px;
+  padding: 32px 0;
   @media screen and (max-width: 480px) {
-    padding: 16px;
+    padding: 16px 0;
   }
 `;
 
@@ -42,9 +44,87 @@ export const AboutContent = styled.div`
   }
 `;
 
-export const AboutH1 = styled.h1`
-  font-size: 32px;
-  text-align: center;
+export const ImgWrap = styled.div`
+  max-width: 555px;
+  height: 100%;
+  margin: auto auto 24px;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  margin: 10px auto;
+`;
+
+export const AboutText = styled.div`
+  max-width: 1100px;
+  margin: 32px auto 0;
+  padding-bottom: 32px;
+  @media screen and (max-width: 480px) {
+    text-align: center;
+  }
+`;
+
+export const TopLine = styled.p`
+  color: #61dafb;
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 700;
+  letter-spacing: 1.4px;
+  text-transform: uppercase;
+  margin-bottom: 16px;
+`;
+
+export const Heading = styled.h4`
   color: #fff;
-  margin-bottom: 32px;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 1.1;
+  margin-bottom: 24px;
+  @media screen and (max-width: 480px) {
+    font-size: 24px;
+  }
+`;
+
+export const AboutP = styled.p`
+  color: #fff;
+  font-size: 20px;
+  line-height: 28px;
+  margin-bottom: 8px;
+  @media screen and (max-width: 480px) {
+    font-size: 18px;
+  }
+`;
+
+export const AboutLink = styled.a`
+  color: #61dafb;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: inherit;
+  cursor: pointer;
+  transition: all .3 ease-out;
+  &:hover {
+    color: #fff;
+    transition: all .3 ease-out;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 16px;
+  }
+`;
+
+export const AboutHighLight = styled.span`
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 18px;
+`;
+
+export const BottomLine = styled.p`
+  color: #61dafb;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: 1.4px;
+  text-transform: uppercase;
+  margin: 8px 0 16px;
 `;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import Svg from '../../assets/images/about_me.svg';
 import {
   Container,
   AboutWrapper,
   Icon,
   AboutContent,
-  AboutH1 } from './AboutElements';
+  ImgWrap,
+  Img,
+  AboutText,
+  TopLine,
+  Heading,
+  AboutP,
+  AboutHighLight,
+  AboutLink,
+  BottomLine } from './AboutElements';
 
 const About = () => {
+
   return (
     <Container>
       <AboutWrapper>
         <Icon to="/">faber</Icon>
         <AboutContent>
-          <AboutH1>About Me</AboutH1>
+          <ImgWrap>
+            <Img src={Svg} alt="my-world" />
+          </ImgWrap>
+          <AboutText>
+            <TopLine>let's start with introduce myself</TopLine>
+            <Heading>I'm Fabrizio a junior frontend developer with passion for web design.</Heading>
+            <AboutP>
+              I'm a Italian ex chef appasionate of technology, with high attention to details that love build and create things.
+              Since 2 years ago that I decide to switch my professional career to the world of programming.
+              So I started study by myself following some on-line courses on web programming in particular.
+              On October 2020 I started a master of software engineering at
+              <AboutLink href="https://www.assemblerschool.com/" target="_blank" aria-label="Assembler"> Assembler </AboutLink>school.
+              <AboutHighLight> I'm a React specialist but also I usually work with all the MERN stack </AboutHighLight>(MongoDB, Express, React, NodeJs).
+            </AboutP>
+            <BottomLine>Feel free to check out my <AboutLink href="/" aria-label="homepage">portfolio</AboutLink>  and <AboutLink href="/contact" aria-label="contactpage">contact me</AboutLink> for any further information, I always glad to answer you.</BottomLine>
+          </AboutText>
         </AboutContent>
       </AboutWrapper>
     </Container>
