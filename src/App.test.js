f2e5869c6f3826cc64137bd0d3d21655947bc83f
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-01 20:25:47 +0200

    feat: add dummy test

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  expect(1).toBe(1);
+});
