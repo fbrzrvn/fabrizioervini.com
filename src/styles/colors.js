b79a73e98396707a2818080eaae857ed46d7f3b5
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-01 19:59:44 +0200

    feat: add global style

---
+++ src/styles/colors.js
@@ -0,0 +1,26 @@
+export const COLOR = {
+  // Light Theme
+  lightBackground: '#F5F6F7',
+  lightNavbar: '#FFFFFF',
+  lightNavbarLink: '#764ABC',
+  lightPrimary: '#7431CA',
+  lightText: '#1C1E21',
+  lightTextSecondary: '#606770',
+  lightFooter: '#F8F9FA',
+  // Dark Theme
+  darkBackground: '#18191A',
+  darkNavbar: '#242526',
+  darkNavbarLink: '#BA8FFF',
+  darkPrimary: '#B97CFD',
+  darkText: '#F5F6F7',
+  darkTextSecondary: '#DADDE1',
+  darkFooter: '#1C1E21',
+  // Primary
+  primaryDarkest: '#730C9A',
+  primaryDarker: '#6D1CAC',
+  primaryDark: '#7431CA',
+  primary: '#BA8FFF',
+  primaryLight: '#B97CFD',
+  primaryLighter: '#CC8FFC',
+  primaryLightest: '#FCF2FF',
+};
---
+++ src/styles/globals.js
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components';
+import { COLOR } from './colors';
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    text-rendering: optimizelegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 18px;
+    line-height: 1;
+    transition: all 1s ease;
+ }
+  @media (prefers-reduced-motion: reduce) {
+    *,
+    *::before,
+    *::after {
+      animation-duration: 0.01ms !important;
+      animation-iteration-count: 1 !important;
+      transition-duration: 0.01ms !important;
+      scroll-behavior: auto !important;
+    }
+  }
+`;
+
+export const lightTheme = {
+  body: COLOR.lightBackground,
+  text: COLOR.lightText,
+  textSecondary: COLOR.lightTextSecondary,
+  primary: COLOR.lightPrimary,
+  navbarBg: COLOR.lightNavbar,
+  navbarLink: COLOR.lightNavbarLink,
+  gradient: COLOR.lightGradient,
+  footer: COLOR.lightFooter,
+};
+
+export const darkTheme = {
+  body: COLOR.darkBackground,
+  text: COLOR.darkText,
+  textSecondary: COLOR.darkTextSecondary,
+  primary: COLOR.darkPrimary,
+  navbarBg: COLOR.darkNavbar,
+  navbarLink: COLOR.darkNavbarLink,
+  gradient: COLOR.darkGradient,
+  footer: COLOR.darkFooter,
+};
