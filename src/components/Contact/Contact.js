5ac9f369639b518d95dfda857754d8ee40593a6a
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-01 20:06:58 +0200

    feat: add contact section

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import BtnLink from '../BtnLink';
+import {
+  ContactBtn,
+  ContactContainer,
+  ContactContent,
+  ContactH1,
+  ContactP,
+} from './styles';
+
+const Contact = () => {
+  return (
+    <ContactContainer id="contact">
+      <ContactContent>
+        <ContactH1>Get in touch</ContactH1>
+        <ContactP>
+          I&apos;m currently available for freelance work. If you have a project
+          that you want to get started or just fancy saying hey, get in touch.
+        </ContactP>
+        <ContactBtn>
+          <BtnLink to="/contact">Contact Me</BtnLink>
+        </ContactBtn>
+      </ContactContent>
+    </ContactContainer>
+  );
+};
+
+export default Contact;
