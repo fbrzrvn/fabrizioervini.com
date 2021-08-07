f91a430550ff435f938474b10befe2927c78bfd2
Fabrizio Ervini <fabri.es018@gmail.com>
2021-08-07 19:21:43 +0200

    fix: toggleNavabr issue

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -9,28 +9,28 @@ import {
 } from './styles';
 
 type SidebarProps = {
-  toggleNavbar: boolean | (() => void);
+  toggleNavbar: boolean | any;
   isOpen: boolean;
 };
 
 const Sidebar = ({ toggleNavbar, isOpen }: SidebarProps) => {
   return (
-    <SidebarContainer isOpen={isOpen} onClick={() => toggleNavbar}>
-      <Icon onClick={() => toggleNavbar}>
+    <SidebarContainer isOpen={isOpen} onClick={() => toggleNavbar()}>
+      <Icon onClick={() => toggleNavbar()}>
         <CloseIcon />
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to="about" onClick={() => toggleNavbar}>
+          <SidebarLink to="about" onClick={() => toggleNavbar()}>
             About
           </SidebarLink>
-          <SidebarLink to="work" onClick={() => toggleNavbar}>
+          <SidebarLink to="work" onClick={() => toggleNavbar()}>
             Work
           </SidebarLink>
-          <SidebarLink to="blog" onClick={() => toggleNavbar}>
+          <SidebarLink to="blog" onClick={() => toggleNavbar()}>
             Blog
           </SidebarLink>
-          <SidebarLink to="contact" onClick={() => toggleNavbar}>
+          <SidebarLink to="contact" onClick={() => toggleNavbar()}>
             Contact
           </SidebarLink>
         </SidebarMenu>
